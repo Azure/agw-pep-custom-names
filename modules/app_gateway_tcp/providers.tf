d2abc390ffc58be94118cade16c510576ddacf90
cmendible <266546+cmendible@users.noreply.github.com>
2024-09-17 11:53:39 +0200

    updated providers configuration

--- modules/app_gateway_tcp/providers.tf
+++ modules/app_gateway_tcp/providers.tf
@@ -2,23 +2,11 @@ terraform {
   required_version = "> 0.14"
   required_providers {
     azurerm = {
-      version = "= 3.83.0"
     }
     azapi = {
-      source  = "azure/azapi"
-      version = "= 1.0.0"
+      source = "azure/azapi"
     }
     random = {
-      version = ">= 3.6.0"
-    }
-  }
-}
-
-provider "azurerm" {
-  skip_provider_registration = true
-  features {
-    key_vault {
-      purge_soft_delete_on_destroy = true
     }
   }
 }
--- providers.tf
+++ providers.tf
@@ -1,11 +1,15 @@
 terraform {
-  required_version = "> 0.14"
+  required_version = ">= 1.4.6"
   required_providers {
     azurerm = {
       version = "= 3.83.0"
     }
     azuread = {
       version = "= 2.44.0"
+    }
+    azapi = {
+      source  = "Azure/azapi"
+      version = "1.13.1"
     }
     random = {
       version = ">= 3.6.0"
@@ -20,6 +24,9 @@ provider "azurerm" {
   skip_provider_registration = true
   features {
     key_vault {
+      purge_soft_delete_on_destroy = true
+    }
+    api_management {
       purge_soft_delete_on_destroy = true
     }
   }
